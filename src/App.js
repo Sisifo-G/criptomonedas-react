2f5b1ec7e8308436914e9909aa1c76a4858969e6
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-03-07 15:19:01 -0500

    styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hey world!</h1>
-      </header>
-    </div>
+    <Contenedor>
+      <h1>Hey world! This is my Critomonedas app</h1>
+    </Contenedor>
   );
 }
 
 export default App;
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
