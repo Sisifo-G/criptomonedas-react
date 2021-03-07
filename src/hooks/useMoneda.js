178ae33325f75fc89962ef0b365844f8972b9758
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-03-07 18:19:51 -0500

    Select Moneda ok

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import styled from "styled-components";
 
 const useMoneda = (label, stateInicial, opciones) => {
   // State del Custom hook
@@ -6,18 +7,38 @@ const useMoneda = (label, stateInicial, opciones) => {
 
   const Seleccionar = () => (
     <Fragment>
-      <label>{label}</label>
-      <select>
+      <Label>{label}</Label>
+      <Select onChange={(e) => actualizarState(e.target.value)} value={state}>
         <option value=""> -- Seleccione -- </option>
         {opciones.map((opcion) => (
           <option key={opcion.codigo} value={opcion.codigo}>
             {opcion.nombre}
           </option>
         ))}
-      </select>
+      </Select>
     </Fragment>
   );
   return [state, Seleccionar, actualizarState];
 };
 
 export default useMoneda;
+
+const Label = styled.label`
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  --webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.3rem;
+`;
