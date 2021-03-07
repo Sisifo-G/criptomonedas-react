5da2b530ec4246544688bb04e920cf8c404bcbbf
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-03-07 18:34:07 -0500

    Customhook useCriptomoneda

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import useMoneda from "../hooks/useMoneda";
+import useCriptomoneda from "../hooks/useCriptomoneda";
 
 const Formulario = () => {
   // Utilizar useMoneda
@@ -14,9 +15,16 @@ const Formulario = () => {
   ];
   const [moneda, SelectMonedas] = useMoneda("Elige tu Moneda", "", MONEDAS);
 
+  // Utilizar useCriptomoneda
+  const [criptomoneda, SelectCripto] = useCriptomoneda(
+    "Elige tu Criptomoneda",
+    ""
+  );
+
   return (
     <form>
       <SelectMonedas />
+      <SelectCripto />
       <Boton type="submit" value="Calcular" />
     </form>
   );
