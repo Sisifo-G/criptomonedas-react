e9dec957d424a43125cc05164b094691c5f7452b
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-03-07 15:43:04 -0500

    Background & Heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import imagen from "./cryptomonedas.png";
 
 function App() {
   return (
     <Contenedor>
-      <h1>Hey world! This is my Critomonedas app</h1>
+      <div>
+        <Imagen src={imagen} alt="Imagen Criptomonedas" />
+      </div>
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+      </div>
     </Contenedor>
   );
 }
@@ -18,5 +24,28 @@ const Contenedor = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.div`
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
   }
 `;
